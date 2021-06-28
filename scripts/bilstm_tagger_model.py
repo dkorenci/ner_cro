2d1c4ce6fdad8763ba93ec23be661fa91ed899ea
dkorenci <damir.korencic@gmail.com>
2021-06-28 13:03:14 +0200

    added separate config params for embedding and lstm widths switched to using PyTorchLSTM

--- scripts/bilstm_tagger_model.py
+++ scripts/bilstm_tagger_model.py
@@ -5,7 +5,10 @@ Implements neural model of the BiLSTM sequence tagger component.
 import spacy
 from typing import List
 
-from thinc.api import Model, chain, with_array, TensorFlowWrapper
+from thinc.api import Model, chain, TensorFlowWrapper, PyTorchLSTM
+from thinc.layers import list2padded, padded2list, with_padded, Softmax, with_list
+from thinc.api import strings2arrays, with_array
+
 from spacy.ml import CharacterEmbed
 from spacy.tokens import Doc
 from thinc.types import Floats2d
@@ -18,9 +21,10 @@ from scripts.nercro_utils import logger as L
 
 # https://spacy.io/usage/processing-pipelines#trainable-components
 @spacy.registry.architectures("bilstm.model.v1")
-def build_model(tok2vec: Model[List[Doc], List[Floats2d]], layers, width, num_labels) \
+def build_model(tok2vec: Model[List[Doc], List[Floats2d]], layers, num_labels, emb_width, lstm_width) \
         -> Model[List[Doc], List[Floats2d]]:
-    return thinc_bilstm_model1(tok2vec, num_labels=num_labels, num_layers=layers, width=width)
+    return thinc_bilstm_torch(tok2vec, num_labels=num_labels, emb_width=emb_width,
+                              lstm_width=lstm_width, num_layers=layers)
 
 def tf_bilstm_model1(num_labels, emb_size = 128, state_size = 32, num_layers = 2):
     # https://keras.io/examples/nlp/bidirectional_lstm_imdb/
@@ -37,10 +41,11 @@ def tf_bilstm_model1(num_labels, emb_size = 128, state_size = 32, num_layers = 2
     model.summary()
     return model
 
-def thinc_bilstm_model1(tok2vec: Model[List[Doc], List[Floats2d]], num_labels: int,
-                        width: int = 64, num_layers = 2, state_size = 64) \
+def thinc_bilstm_tf(tok2vec: Model[List[Doc], List[Floats2d]], num_labels: int,
+                    width: int = 64, num_layers = 2, state_size = 64) \
         -> Model[List[Doc], List[Floats2d]]:
     '''
+    Create BiLSTM model based on TF implementation
     :param tok2vec: spacy-compliant token 2 embedding encoder
     :param width: size of the embedding vector
     :param num_layers: number of BiLSTM layers
@@ -50,9 +55,22 @@ def thinc_bilstm_model1(tok2vec: Model[List[Doc], List[Floats2d]], num_labels: i
     '''
     wrapped_tf_model = TensorFlowWrapper(tf_bilstm_model1(num_labels, width, state_size, num_layers))
     #char_embed = CharacterEmbed(width, embed_size, nM, nC, include_static_vectors=False)
-    model = chain(tok2vec, with_array(wrapped_tf_model))
+    #model = chain(tok2vec, list2padded(), wrapped_tf_model, padded2list())
+    model = chain(tok2vec, with_padded(wrapped_tf_model))
+    return model
+
+def thinc_bilstm_torch(tok2vec: Model[List[Doc], List[Floats2d]], num_labels: int, emb_width:int,
+                       lstm_width: int = 64, num_layers: int = 2) \
+    -> Model[List[Doc], List[Floats2d]]:
+    '''
+    Create a BiLSTM model based on pyTorch implementation
+    :return:
+    '''
+    bilstm = PyTorchLSTM(nO=lstm_width*2, nI=emb_width, bi=True, depth=num_layers)
+    classification = Softmax(nO=num_labels, nI=lstm_width)
+    model = chain(tok2vec, with_padded(bilstm), classification)
     return model
 
 if __name__ == '__main__':
     #tf_bilstm_model1()
-    thinc_bilstm_model1()
+    thinc_bilstm_tf()
